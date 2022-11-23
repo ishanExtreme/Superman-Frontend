61cee7af5bf5dbf1ec24efd7ece53b316619761c
Ishan Mishra <ishanmishra@Ishans-MacBook-Pro.local>
2022-11-23 13:31:32 +0530

    preview modal updated

--- src/Components/ModalParent.tsx
+++ src/Components/ModalParent.tsx
@@ -44,9 +44,7 @@ export default function CreateForm(props:{
                 leaveFrom="opacity-100 translate-y-0 sm:scale-100"
                 leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
               >
-                <div className="relative inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
-                  <div className="bg-red-700 px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
-                    <div className="sm:flex sm:items-start">
+                <div className="border-2 border-beta-700 relative p-3 inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
                       <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
                         <Dialog.Title as="h3" className="text-lg font-semibold leading-6 text-center mb-5 text-yellow-400">
                           {props.title}
@@ -55,9 +53,8 @@ export default function CreateForm(props:{
                             {props.children}
                         </div>
                       </div>
-                    </div>
-                  </div>
-                  <div className="bg-red-700 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
+                 
+                  <div className="bg-white px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
                     <button
                       type="button"
                       className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-red-600 text-base font-medium text-white hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:ml-3 sm:w-auto sm:text-sm"
