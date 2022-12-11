0f441184a1a226a30dfa37481a2ad3a3ebeba1e5
ishanExtreme <462_bt19@iiitkalyani.ac.in>
2022-12-11 10:42:59 +0530

    user type updated

--- src/Pages/Profile.tsx
+++ src/Pages/Profile.tsx
@@ -10,6 +10,11 @@ export default function Profile(props:{currentUser:User}) {
     const handleChangePasswordClick = ()=>{
         navigate("/change-password")
     }
+
+    const handleEnableWhatsappClick = ()=>{
+        navigate("/configure-whatsapp")
+    }
+
     return(
         <>
         <NavbarResponsive user={props.currentUser} page="User"/>
@@ -26,11 +31,11 @@ export default function Profile(props:{currentUser:User}) {
                     </button>
 
                     <button 
-                    // onClick={()=>changePasswordHelper(setLoading, oldPass, newPass)} 
+                    onClick={handleEnableWhatsappClick} 
                     type="button" 
                     className="h-10 px-6 pt-2.5 pb-2 bg-alpha-400 text-black font-medium text-xs leading-normal uppercase rounded shadow-md hover:bg-alpha-500 hover:shadow-lg focus:bg-alpha-500 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-alpha-600 active:shadow-lg transition duration-150 ease-in-out flex align-center items-center">
                         <ChatIcon className="h-5 w-5 mr-2" />
-                        Enable Whatsapp Feature
+                        Configure Whatsapp Feature
                     </button>
 
             </div>
