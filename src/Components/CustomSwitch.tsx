fd7d570eded708e2aa58aea7c5e023779c84b64c
ishanExtreme <462_bt19@iiitkalyani.ac.in>
2022-12-11 10:44:05 +0530

    reusable switch added

--- src/Components/CustomSwitch.tsx
+++ src/Components/CustomSwitch.tsx
@@ -1,13 +1,27 @@
 import React from "react";
 
-export default function CustomSwitch() {
+export default function CustomSwitch(
+    props:{
+        title: string,
+        enable: boolean,
+        handleSwitchCB: ()=>void
+    }
+) {
 
     return(
 
     <div className="flex justify-center">
         <div className="form-check form-switch">
-            <input className="form-check-input appearance-none w-9 -ml-10 rounded-full float-left h-5 align-top bg-white bg-no-repeat bg-contain bg-gray-300 focus:outline-none cursor-pointer shadow-sm" type="checkbox" role="switch" id="flexSwitchCheckDefault"/>
-            <label className="form-check-label inline-block text-gray-800" htmlFor="flexSwitchCheckDefault">Default switch checkbox input</label>
+            {props.enable?
+            <input 
+            onClick={props.handleSwitchCB}
+            className="form-check-input appearance-none w-9 -ml-10 rounded-full float-left h-5 align-top bg-no-repeat bg-contain bg-gray-300 focus:outline-none cursor-pointer shadow-sm" type="checkbox" role="switch" id="flexSwitchCheckDefault" checked={true}/>
+            :
+            <input
+            onClick={props.handleSwitchCB}
+            className="form-check-input appearance-none w-9 -ml-10 rounded-full float-left h-5 align-top bg-no-repeat bg-contain bg-gray-300 focus:outline-none cursor-pointer shadow-sm" type="checkbox" role="switch" id="flexSwitchCheckDefault"/>
+            }
+            <label className="form-check-label inline-block text-gray-600" htmlFor="flexSwitchCheckDefault">{props.title}</label>
         </div>
     </div>
 
