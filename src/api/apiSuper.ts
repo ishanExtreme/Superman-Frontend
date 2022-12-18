b770d1de931aada1be89866d1d3fdd394a7f4545
ishanExtreme <462_bt19@iiitkalyani.ac.in>
2022-12-19 03:17:17 +0530

    api host updated

--- src/api/apiSuper.ts
+++ src/api/apiSuper.ts
@@ -2,7 +2,7 @@ import { BoardCreateApi, StageCreateApi, TaskCreateApi } from "../types/api/task
 import { UserLoginApi, UserRegisterApi } from "../types/api/user";
 import { triggerToast } from "../utils/notification";
 
-const API_BASE_URL = "http://127.0.0.1:8000/api/"
+const API_BASE_URL = "https://web-production-cff9.up.railway.app/api/"
 
 type RequestMethod = 'POST' | 'GET' | 'PATCH' | 'DELETE' | 'PUT'
 
